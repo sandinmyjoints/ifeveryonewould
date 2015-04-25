39de874123d6d7d3a44938fd4e52dbb339d932b3
William Bert <william.bert@gmail.com>
2015-04-25 14:24:44 -0700

    Describe signals.

--- index.js
+++ index.js
@@ -167,4 +167,21 @@ function connect() {
   });
 }
 
+function terminator(sig){
+  if (typeof sig === "string") {
+    console.log('%s: Received %s - terminating sample app ...',
+                Date(Date.now()), sig);
+    process.exit(1);
+  }
+  console.log('%s: Node server stopped.', Date(Date.now()) );
+};
+
+process.on('exit', function() { terminator(); });
+
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+ 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+].forEach(function(element, index, array) {
+  process.on(element, function() { terminator(element); });
+});
+
 connect();
